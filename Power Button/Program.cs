f885b8827b32ea32ccfcae7283ea3d4caa2c8945
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-10-30 16:05:30 +0600

    Add (project source code) :: Power Button Options Project
    Features
    
    1. Shutdown
    2. Restart
    3. Sleep
    4. LogOff
    5. Lock
    6. Hibernate
    
    This is a console app
    
    and all the method are reuseable and static

---
+++ Power Button/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Power_Button
+{
+    class PowerButtonOptions
+    {
+        // to logoff or lock import the following dll file
+        [DllImport("user32")]
+
+        // to log off
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("user32")]
+        // to lock 
+        public static extern void LockWorkStation();
+
+        // to put your computer in Hibernate or Sleep, you need the smae DllImport statement for them 
+        [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        public static void Shutdown()
+        {
+            // starts the shutdown application
+            // the argument /s is to shutdown the computer
+            // the argument /t is to tell the process that
+            // the specified operation needs to be completed
+            // after 0 seconds
+
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        public static void Restart()
+        {
+            // starts the Restart application
+            // the argument /r is to restart the computer
+            // the argument /t is to tell the process that
+            // the specified operation needs to be completed
+            // after 0 seconds
+
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        
+
+        public static void LogOff()
+        {
+            ExitWindowsEx(0, 0);
+        }
+
+        public static void Lock()
+        {
+            LockWorkStation();
+        }
+
+        public static void Sleep()
+        {
+            SetSuspendState(false, true, true);
+        }
+        public static void Hibernate()
+        {
+            SetSuspendState(true, true, true);
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Lock();
+                // ShutDown();
+                // Restart();
+                // Hibernate();
+                // Sleep();
+            }
+            catch
+            {
+                Console.Write("Catch Block");
+            }
+
+        }
+
+}
+}
